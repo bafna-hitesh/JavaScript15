bfa19eac4f0876bd45e2a1308f88a949aa1de4ef
hitesh jain <hitesh.bafna00@gmail.com>
2022-02-21 13:22:01 +0530

    feat: add todos in local storage

--- 08_todo_app/script.js
+++ 08_todo_app/script.js
@@ -2,12 +2,27 @@ const form = document.getElementById("form");
 const input = document.querySelector(".todo-input");
 const todos = document.querySelector(".todos");
 
+const todoList = JSON.parse(localStorage.getItem("todos"));
+
+if (todoList) {
+  todoList.forEach((todo) => {
+    addTodo(todo);
+  });
+}
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const todoEle = document.createElement("li");
-  const todoText = input.value;
+  addTodo();
+});
 
+function addTodo(todo) {
+  let todoText = input.value;
+  if (todo) {
+    todoText = todo.text;
+  }
+
+  const todoEle = document.createElement("li");
   if (todoText) {
     todoEle.innerText = todoText;
     todos.appendChild(todoEle);
@@ -16,11 +31,28 @@ form.addEventListener("submit", (e) => {
 
   todoEle.addEventListener("click", () => {
     todoEle.classList.toggle("completed");
+    updateLocalStorage();
   });
 
   todoEle.addEventListener("contextmenu", (e) => {
     e.preventDefault();
-
     todoEle.remove();
+    updateLocalStorage();
   });
-});
+
+  updateLocalStorage();
+}
+
+function updateLocalStorage() {
+  const todos = [];
+  const notesEL = document.querySelectorAll("li");
+
+  notesEL.forEach((notes) => {
+    todos.push({
+      text: notes.innerText,
+      completed: notes.classList.contains("completed"),
+    });
+  });
+
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
